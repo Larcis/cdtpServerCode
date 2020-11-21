8b067c053e120f9d45594ed849e5570abd34ced7
larcis <uygarkoroglu@outlook.com>
2020-11-21 05:08:17 +0300

    database added

--- constants.js
+++ constants.js
@@ -1,4 +1,6 @@
 "use strict";
 
 exports.PORT = 3000;
-exports.DB_CONNECTION_STRING = "mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb";
+//exports.DB_CONNECTION_STRING = "mongodb://127.0.0.1:27017/?compressors=zlib&gssapiServiceName=mongodb";
+
+exports.DB_CONNECTION_STRING = "mongodb+srv://uygar:uygar123@cluster0.tzedi.mongodb.net/cdtp_db?retryWrites=true&w=majority";
